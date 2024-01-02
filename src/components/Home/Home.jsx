1a619ae3e4a0c097731794fba79f637466cccf6d
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-01-02 17:36:19 -0500

    Updated the home page except for the icons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,9 +6,14 @@ const Home = () => {
     <div className= {styles.container}>
         <h1><span>Mohammad</span> El Prince</h1>
         <p>This is my official portfolio website to showcase my web development activities</p>
-        <a href="">Download Resume</a>
+        <a href="" className={styles.download}>Download Resume</a>
         <div className={styles.image_container}>
             <img src='/pattern.png' className={styles.background_pattern}/>
+            <img src='/myimage.png' className={styles.my_image}/>
+        </div>
+        <div className={styles.social_links}>
+          <a href=""></a>
+          <a href=""></a>
         </div>
         
     </div>
